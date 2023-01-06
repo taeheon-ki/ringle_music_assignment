3dd1cde382e4082db5047efc52cdebe6add31b8c
taeheon.ki <taeheon.ki@ringleplus.com>
2023-01-06 09:56:58 +0900

    add query to search group_musics

--- app/services/groups/get_groups_service.rb
+++ app/services/groups/get_groups_service.rb
@@ -1,10 +1,14 @@
 module Groups
     class GetGroupsService < ApplicationService
-        def initialize()
+        def initialize(**args)
+            @query = args[:query] if args.key?(:query)
         end
 
         def call
             groups = Group.all
+            filtered_groups = groups.where(
+                "group_name LIKE :like_query ",
+                like_query: "%#{@query}%")
 
         end
     end
